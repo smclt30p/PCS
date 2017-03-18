ba4785f1e962cb8d38459c5954d322b6771379a8
Ognjen Galić <smclt30p@gmail.com>
2017-03-18 02:46:53 +0100

    Revamp About dialog

--- ui/Ui_AboutDialog.py
+++ ui/Ui_AboutDialog.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file '.\ui\src\about.ui'
+# Form implementation generated from reading ui file 'ui\src\about.ui'
 #
 # Created by: PyQt5 UI code generator 5.8.1
 #
@@ -11,11 +11,19 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_AboutDialog(object):
     def setupUi(self, AboutDialog):
         AboutDialog.setObjectName("AboutDialog")
-        AboutDialog.resize(300, 151)
+        AboutDialog.resize(300, 187)
         AboutDialog.setMinimumSize(QtCore.QSize(300, 0))
-        self.verticalLayout = QtWidgets.QVBoxLayout(AboutDialog)
-        self.verticalLayout.setContentsMargins(20, 20, 20, 20)
-        self.verticalLayout.setSpacing(8)
+        self.verticalLayout_2 = QtWidgets.QVBoxLayout(AboutDialog)
+        self.verticalLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout_2.setSpacing(0)
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.bg = QtWidgets.QLabel(AboutDialog)
+        self.bg.setText("")
+        self.bg.setPixmap(QtGui.QPixmap("../res/logo.png"))
+        self.bg.setObjectName("bg")
+        self.verticalLayout_2.addWidget(self.bg)
+        self.verticalLayout = QtWidgets.QVBoxLayout()
+        self.verticalLayout.setContentsMargins(6, 6, 6, 16)
         self.verticalLayout.setObjectName("verticalLayout")
         self.label = QtWidgets.QLabel(AboutDialog)
         font = QtGui.QFont()
@@ -25,16 +33,22 @@ class Ui_AboutDialog(object):
         self.label.setAlignment(QtCore.Qt.AlignCenter)
         self.label.setObjectName("label")
         self.verticalLayout.addWidget(self.label)
-        self.label_2 = QtWidgets.QLabel(AboutDialog)
-        self.label_2.setAlignment(QtCore.Qt.AlignCenter)
-        self.label_2.setObjectName("label_2")
-        self.verticalLayout.addWidget(self.label_2)
+        self.version_string = QtWidgets.QLabel(AboutDialog)
+        font = QtGui.QFont()
+        font.setBold(True)
+        font.setWeight(75)
+        self.version_string.setFont(font)
+        self.version_string.setText("")
+        self.version_string.setAlignment(QtCore.Qt.AlignCenter)
+        self.version_string.setObjectName("version_string")
+        self.verticalLayout.addWidget(self.version_string)
         spacerItem = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Minimum)
         self.verticalLayout.addItem(spacerItem)
         self.label_3 = QtWidgets.QLabel(AboutDialog)
         self.label_3.setAlignment(QtCore.Qt.AlignCenter)
         self.label_3.setObjectName("label_3")
         self.verticalLayout.addWidget(self.label_3)
+        self.verticalLayout_2.addLayout(self.verticalLayout)
 
         self.retranslateUi(AboutDialog)
         QtCore.QMetaObject.connectSlotsByName(AboutDialog)
@@ -43,6 +57,5 @@ class Ui_AboutDialog(object):
         _translate = QtCore.QCoreApplication.translate
         AboutDialog.setWindowTitle(_translate("AboutDialog", "PCS - About"))
         self.label.setText(_translate("AboutDialog", "PCS - Putlocker Client System"))
-        self.label_2.setText(_translate("AboutDialog", "Version 1.0.0"))
         self.label_3.setText(_translate("AboutDialog", "Copyright (C) 2017 Ognjen Galic"))
 
